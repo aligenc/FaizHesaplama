ad68461fcf9ebcb10008f98b2483e7e7f784878a
ali.genc <=>
2017-10-07 18:22:35 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FaizHesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
